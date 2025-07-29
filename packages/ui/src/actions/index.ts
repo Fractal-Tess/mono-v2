29de481df935263166c298100b18006255508dd1
Fractal-Tess <vgfractal@gmail.com>
2025-07-29 22:43:26 +0300

    Create a shared shadcn svelte ui lib

---
+++ packages/ui/src/actions/index.ts
@@ -0,0 +1,4 @@
+export * from './click-outside.svelte';
+export * from './clipboard.svelte';
+export * from './debounce.svelte';
+export * from './hover.svelte';
---
+++ packages/ui/src/components/icons/index.ts
@@ -0,0 +1,34 @@
+import Apple from './apple.svelte';
+import GitHub from './github.svelte';
+import Google from './google.svelte';
+import Loading from './loading.svelte';
+import Logo from './logo.svelte';
+import PayPal from './paypal.svelte';
+import Spinner from './spinner.svelte';
+import Summary from './summary.svelte';
+import Translate from './translate.svelte';
+import Twitter from './twitter.svelte';
+
+export {
+  Apple,
+  GitHub,
+  Google,
+  Logo,
+  PayPal,
+  Twitter,
+  Spinner,
+  Summary,
+  Translate,
+  Loading,
+  //
+  Apple as IconApple,
+  GitHub as IconGitHub,
+  Google as IconGoogle,
+  Logo as IconLogo,
+  PayPal as IconPayPal,
+  Twitter as IconTwitter,
+  Spinner as IconSpinner,
+  Summary as IconSummary,
+  Translate as IconTranslate,
+  Loading as IconLoading,
+};

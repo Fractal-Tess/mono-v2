7e026b3808bc54f82f6f131ba7eb7f07e2910184
Fractal-Tess <vgfractal@gmail.com>
2025-07-31 22:20:40 +0300

    feat: add theme toggle button to NavUser component for dark mode functionality

--- apps/web/src/routes/(protected)/_components/NavUser.svelte
+++ apps/web/src/routes/(protected)/_components/NavUser.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+  import { MoonIcon, SunIcon } from 'lucide-svelte';
+  import { toggleMode } from 'mode-watcher';
   import { useClerkContext, UserButton } from 'svelte-clerk/client';
+  import { Button } from '@repo/ui/components/ui/button/index.js';
   import * as Sidebar from '@repo/ui/components/ui/sidebar/index.js';
 
   const clerk = useClerkContext();
@@ -19,6 +22,15 @@
           {clerk.user?.primaryEmailAddress?.emailAddress || 'No email'}
         </span>
       </div>
+      <Button onclick={toggleMode} variant="outline" size="icon">
+        <SunIcon
+          class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 !transition-all dark:-rotate-90 dark:scale-0"
+        />
+        <MoonIcon
+          class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 !transition-all dark:rotate-0 dark:scale-100"
+        />
+        <span class="sr-only">Toggle theme</span>
+      </Button>
 
       <UserButton
         afterSignOutUrl="/sign-in"
